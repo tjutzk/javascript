6534164d97408c72b4a181f89728f9b8cc6118ad
DESKTOP-D7LA231\zk <1356476048@qq.com>
2020-10-22 18:01:00 +0800

    call实现

--- call_apply_bind.js
+++ call_apply_bind.js
@@ -2,7 +2,17 @@
 function test() {
     console.log(this.name)
 }
+Function.prototype.callme = function () {
+    let fn = Symbol()
+    let ctx = arguments[0] || window
+    ctx[fn] = this
+    let args = []
+    for (let i = 1; i < arguments.length; i++) {
+        args.push(arguments[i])
+    }
+    return ctx[fn](...args)
+}
 var testObject = {
     name:'call实现'
 }
-test.call(testObject)
+test.callme(testObject)
