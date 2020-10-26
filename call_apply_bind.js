8f6b7470d7e2a8938f9d71de0f33d60aef04806a
DESKTOP-D7LA231\zk <1356476048@qq.com>
2020-10-26 10:01:13 +0800

    更新apply、bind

--- call_apply_bind.js
+++ call_apply_bind.js
@@ -1,5 +1,6 @@
 // call实现
-function test() {
+function test(value) {
+    console.log(value)
     console.log(this.name)
 }
 Function.prototype.callme = function () {
@@ -10,9 +11,55 @@ Function.prototype.callme = function () {
     for (let i = 1; i < arguments.length; i++) {
         args.push(arguments[i])
     }
-    return ctx[fn](...args)
+    let res =  ctx[fn](...args)
+    delete ctx[fn]
+    return res
 }
 var testObject = {
-    name:'call实现'
+    name:'call实现',
+    applyName:'apply实现',
+    bindName:'bind实现'
 }
-test.callme(testObject)
+test.callme(testObject, '-----')
+
+// apply实现
+function applyTest(value) {
+    console.log(value)
+    console.log(this.applyName)
+}
+Function.prototype.applyme = function () {
+    let fn = Symbol()
+    let ctx = arguments[0] || window
+    ctx[fn] = this
+    let args = []
+    if(arguments[1] &&  Object.prototype.toString.call(arguments[1]) === "[object Array]"){
+        args = arguments[1]
+    }else{
+        console.log('参数不正确')
+        return new Error('参数不正确')
+    }
+    let res =  ctx[fn](...args)
+    delete ctx[fn]
+    return res
+}
+applyTest.applyme(testObject,['-----'])
+
+// bind实现
+function bindTest(value) {
+    console.log(value)
+    console.log(this.bindName)
+}
+Function.prototype.bindme = function () {
+    let fn = Symbol()
+    let ctx = arguments[0] || window
+    ctx[fn] = this
+    let args = []
+    for (let i = 1; i < arguments.length; i++) {
+        args.push(arguments[i])
+    }
+    let res =  function () {
+        return ctx[fn](...args)
+    }
+    return res
+}
+bindTest.bindme(testObject,'-----')()
