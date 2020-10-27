833cd052c2e5c6fafb29f429ee6a4272508de5da
DESKTOP-D7LA231\zk <1356476048@qq.com>
2020-10-27 17:55:29 +0800

    js原型组合继承

--- js原型原型链/jicheng.js
+++ js原型原型链/jicheng.js
@@ -14,8 +14,9 @@ function People(name){
 People.prototype.sayName = function () {
     console.log('我是' + this.name)
 }
-// 原型链继承
-/*缺点：
+/*
+原型链继承
+缺点：
 引用类型的属性被所有实例共享
 在创建Child 的实例时， 不能向Person传参
 */
@@ -28,8 +29,9 @@ man1.info.id = '111'
 console.log(man1.info.id) //111
 console.log(man2.info.id) //111与man1一样
 
-// 借用构造函数继承
-/*缺点：
+/*
+借用构造函数继承
+缺点：
 引用类型的属性被所有实例共享
 在创建Child 的实例时， 不能向Person传参
 */
@@ -54,3 +56,26 @@ console.log(child1.colors); // ["red", "blue", "green", "yellow"]
 console.log(child2.colors); // ["red", "blue", "green"]
 console.log(child1 instanceof Child) //true
 console.log(child1 instanceof Person) //false
+
+/*
+组合继承
+缺点：调用了两次父类构造函数
+*/
+function animal(name) {
+    this.name = name || 'tempName'
+    this.color = ['blur', 'red','yellow']
+}
+animal.prototype.sayName = function(){
+    console.log('我是' + this.name)
+}
+function cat(name) {
+    animal.call(this, name)
+}
+cat.prototype = new animal()
+let cat1 = new cat('cat1')
+let cat2 = new cat('cat2')
+cat1.color.push('green')
+cat1.sayName()
+cat2.sayName()
+console.log(cat1.color)
+console.log(cat2.color)
